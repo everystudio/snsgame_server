d9be0eff04b0759dce9e79e8995d55ce715978a3
EC2 Default User <ec2-user@ip-10-0-1-8.ap-northeast-1.compute.internal>
2021-06-12 07:08:27 +0000

    first commit

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistrationController extends Controller
+{
+	public function Start(Request $request){
+		$response = array();
+
+		return $response;
+	}
+}
